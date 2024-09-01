768ae29433189f1c97db6828896f222aaea24f61
rosstimo <rosstimo@isu.edu>
2024-09-01 07:28:13 -0600

    fix venv script and update README.md

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # Robot
 Robot Project
+
+## Setup
+
+### Python Virtual Environment
+
+You may need to install python3-full
+`sudo apt install python3-full`
+Create the venv
+`python3 -m venv ~/python/venv`
+Start the python virtual envirnment
+`source ~/python/venv/bin/activate`
+Install the required libraries
+`pip install -r requirments.txt`
+Run your app
+`~/python/venv/bin/python app.py`
+Finally, deactivate the virtual environment when you are done
+`deactvate`
+
+
+Optionally use the shell script to start the python virtual envirnment by sourcing the start_venv.sh
+`source start_venv.sh`
+
+this will also install any libraries listed in requirments.txt
--- start_venv.sh
+++ start_venv.sh
@@ -1,17 +1,8 @@
 #!/bin/bash
 
 # Start the python virtual environment
-echo "Starting python virtual environment"
-echo "If this does not work, then you may need to install python3-venv"
-# if ~/python/venv/bin/activate exists, then activate the virtual environment
-# otherwise, create the virtual environment and activate it
-
-if [ -f ~/python/venv/bin/activate ]; then
-    source ~/python/venv/bin/activate
-else
-    python3 -m venv ~/python/venv
-    source ~/python/venv/bin/activate
-fi
+python3 -m venv ~/python/venv &&
+source ~/python/venv/bin/activate &&
 
 # if requirements.txt exists, then install the requirements
 if [ -f requirements.txt ]; then
@@ -19,10 +10,7 @@ if [ -f requirements.txt ]; then
 fi
 
 # add ~/python/venv/bin to the beginning of PATH so that the python in the virtual environment is used
-
 export PATH=~/python/venv/bin:$PATH
 
 # if the virtual environment is activated, then the prompt will start with (venv)
-
-echo "venv activated"
-echo "run the command: 'deactivate' to exit the virtual environment"
+echo "run: 'deactivate' to exit the virtual environment"
